87616a7233a10f920d23cbf3db7294c26f5a971c
Vahhid Lelissa <vahhid@gmail.com>
2023-08-07 18:36:38 +0300

    Miles to kilometers and vice versal

--- hello_World.py
+++ hello_World.py
@@ -7,3 +7,12 @@ first_side = 4.0
 second_side = 3.0
 hypotenous = (first_side**2 + second_side**2)**0.5
 print("The hypoteous is "+ str(hypotenous))
+#Kilometer to miles and vice versal conversion
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = kilometers / 1.61
+kilometers_to_miles = miles * 1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
