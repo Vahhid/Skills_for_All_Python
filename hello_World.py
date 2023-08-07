4856604af389d827c5fc76815f74504c89bfd27b
Vahhid Lelissa <vahhid@gmail.com>
2023-08-07 18:21:29 +0300

    Pythagoras triangle with float to string casting

--- hello_World.py
+++ hello_World.py
@@ -2,3 +2,8 @@ print("Hello World")
 #Frist variable
 first_variable = 10
 print("Hello world" * first_variable)
+#Finding the hypotenous
+first_side = 4.0
+second_side = 3.0
+hypotenous = (first_side**2 + second_side**2)**0.5
+print("The hypoteous is "+ str(hypotenous))
