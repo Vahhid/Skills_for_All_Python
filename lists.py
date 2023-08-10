ede7e8842319a66166e22811048aba4d05f40e98
Vahhid Lelissa <vahhid@gmail.com>
2023-08-10 15:05:52 +0300

    list basics

---
+++ lists.py
@@ -0,0 +1,16 @@
+#lists
+numbers = [10,5,7,2,1]
+#print("original contents of numbers",numbers)
+#numbers[0]=111
+#print("changed contents of numbers",numbers)
+#del numbers[0]
+#print(len(numbers))
+#print("After delete: ",numbers)
+#out of range
+#print(numbers[4])
+#Negative index
+#print(numbers[-2])
+numbers.append("Appended")
+print(numbers)
+numbers.insert(1,"inserted")
+print(numbers)
