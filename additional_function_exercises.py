9a529488c5deec0e1a9846f6913ac6ff9d39ec83
Vahhid Lelissa <vahhid@gmail.com>
2023-08-12 11:52:56 +0300

    Triangle checks and area

---
+++ additional_function_exercises.py
@@ -0,0 +1,30 @@
+def is_triangle(a, b, c):
+    return a + b > c and a + c > b and b + c > a
+#print("Is 1, 3, 4 and triangle? ", is_triangle(1, 3, 4))
+
+def is_right_angled_triangle (a, b, c):
+    if (is_triangle(a,b,c)):
+        if (a > b and a > c):
+            return a **2 == b **2 + c **2
+        elif (b > a and b > c):
+            return b**2 == a **2 + c**2
+        else:
+            return c**2 == a **2 + b**2
+
+    else:
+        print("This is not a tringle")
+        return None
+#print("Is 1, 1, 4 a right angled triange?", is_right_angled_triangle(1, 1, 4))
+#print("Is 1, 1, 1 a right angled triange?", is_right_angled_triangle(1, 1, 1))
+#print("Is 3, 4, 5 a right angled triange?", is_right_angled_triangle(3, 4, 5))
+def area_triangle (a, b, c):
+    if (is_triangle(a,b,c)):
+        s = ( a + b + c)
+        return (s *(s-a )+ (s-b)+ (s-c))**0.5
+    else:
+        print("This is not a tringle")
+        return None  
+
+print("Area of 1, 1, 4 is?", area_triangle(1, 1, 4))
+print("Area of 1, 1, 1 is?", area_triangle(1, 1, 1))
+print("Area of 3, 4, 5 is?", area_triangle(3, 4, 5))
