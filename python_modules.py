b6713459cdb045392e07539f2121a56305a90759
Vahhid Lelissa <vahhid@gmail.com>
2023-08-16 03:09:06 +0300

    math pi, radians and degrees checked

---
+++ python_modules.py
@@ -0,0 +1,15 @@
+import math
+"""
+for name in dir(math):
+    print(name, end = "\t")
+"""
+#print(math.pi)
+"""
+for i in range(10):
+    print (i * math.pi , "in radians is ", \
+           math.degrees( i * math.pi) , "in degrees")
+"""
+for i in range(0,361,5):
+    print( i , "in degrees is ",\
+          round(math.radians(i)/math.pi,2),\
+          " PI radians")
