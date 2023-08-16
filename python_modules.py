efa1a267b4eae60f291b67c46bf68ac132bc1cce
Vahhid Lelissa <vahhid@gmail.com>
2023-08-16 03:17:15 +0300

    math sine function

--- python_modules.py
+++ python_modules.py
@@ -8,8 +8,15 @@ for name in dir(math):
 for i in range(10):
     print (i * math.pi , "in radians is ", \
            math.degrees( i * math.pi) , "in degrees")
-"""
+
 for i in range(0,361,5):
     print( i , "in degrees is ",\
           round(math.radians(i)/math.pi,2),\
           " PI radians")
+
+          
+"""
+for i in range(0, 361,5):
+    print("Sine of ", i, " degrees or ", \
+          round(math.radians(i)/math.pi,2), "PI radians is "
+          ,round(math.sin(math.radians(i)),2))
