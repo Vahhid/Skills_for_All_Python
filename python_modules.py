6e750bfc6d39285ef6405aa64480b635914d23f9
Vahhid Lelissa <vahhid@gmail.com>
2023-08-16 04:19:37 +0300

    Platform module

--- python_modules.py
+++ python_modules.py
@@ -1,4 +1,4 @@
-import math, random
+import math, random, platform
 """
 for name in dir(math):
     print(name, end = "\t")
@@ -27,9 +27,20 @@ print("\n" *5)
 for i in range(5):
     print(random.random())
           ,round(math.sin(math.radians(i)),2))
-"""
+
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print(random.choice(my_list))
 print(random.sample(my_list, 5))
 print(random.sample(my_list, 10))
+"""
+print(platform.platform())
+
+print(platform.platform(aliased= True, terse= True))
+
+print(platform.machine())
+print(platform.processor())
+print(platform.system())
+print(platform.version())
+print(platform.python_implementation())
+print(platform.python_version_tuple())
