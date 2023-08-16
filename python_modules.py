4de7feff9ce605e7c2bce43a6f37077e81c9b691
Vahhid Lelissa <vahhid@gmail.com>
2023-08-16 04:07:30 +0300

    Choice and sample functions

--- python_modules.py
+++ python_modules.py
@@ -19,11 +19,17 @@ for i in range(0,361,5):
 for i in range(0, 361,5):
     print("Sine of ", i, " degrees or ", \
           round(math.radians(i)/math.pi,2), "PI radians is "
-          ,round(math.sin(math.radians(i)),2))
-"""
+
 for i in range(5):
     print(random.random())
 random.seed(0)
 print("\n" *5)
 for i in range(5):
     print(random.random())
+          ,round(math.sin(math.radians(i)),2))
+"""
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(random.choice(my_list))
+print(random.sample(my_list, 5))
+print(random.sample(my_list, 10))
