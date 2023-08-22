d521c5a8cf6623f66f3855a5cc33a9c2dd4697ed
Vahhid Lelissa <vahhid@gmail.com>
2023-08-22 13:12:58 +0300

    String methods

---
+++ string_methods.py
@@ -0,0 +1,24 @@
+myParagraph = """Let's go through some standard Python string 
+methods. We're going to go through them in alphabetical order 
+ to be honest, any order has as many disadvantages as advantages,
+ so the choice may as well be random.
+"""
+shortStr = "ALL CAPS"
+
+#print(shortStr.capitalize())
+#print(myParagraph.capitalize())
+toCenter = "gong"
+#print(toCenter +"end")
+#print(toCenter.center(20,"/")+"end")
+#print(toCenter.endswith("ng"))
+#print(myParagraph.find("gong"))
+#print("gong" in myParagraph)
+#print(shortStr.find("A",1))
+myNum = 123
+#print(myNum.isalnum())
+#print("123".isalnum())
+#print("Gong".isalnum())
+#print("123Gong".isalnum())
+print("123".islower())
+print("gong".islower())
+print("gong123".islower())
