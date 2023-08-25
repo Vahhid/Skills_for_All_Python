5f395589c1d7c0f239709b9a40bc1dec68893534
Vahhid Lelissa <vahhid@gmail.com>
2023-08-25 20:44:07 +0300

    Mapping compacted into a two dimentional array

--- seven_segment_display.py
+++ seven_segment_display.py
@@ -2,25 +2,34 @@
 #decimals = list(range(10))
 #print(decimals)
 #Function for displaying individual decimals
-def draw(decimal):
+def draw(z):
     def horizontal(side):
-        if side =="a":
+        if side == 1:
             print(8*"#")
-        elif side == "b":
+        elif side == 2:
             print(" "," "*4 , "#")
-        elif side == "c":
+        elif side == 3:
             print("#"," "*4 , "#")
     def vertical(side):
-        if side == "x":
+        if side == 1:
             for i in range(3):
                 print("#",4*" ","#")
-        elif side == "y":
+        elif side == 2:
             for i in range(3):
                 print(" ",4*" ","#")
-        elif side == "z":
+        elif side == 3:
             for i in range(3):
                 print("#",4*" "," ")
-          
+    mp = [[1,1,3,1,1], [2,2,2,2,2], [1,2,1,3,1], [1,2,1,2,1], [3,1,1,2,2], \
+            [1,3,1,2,1], [1,2,1,1,1], [1,2,2,2,2], [1,1,1,1,1], [1,1,1,2,2] ]  
+    horizontal(mp[z][0])
+    vertical(mp[z][1])
+    horizontal(mp[z][2])
+    vertical(mp[z][3])
+    horizontal(mp[z][4])
+    print(" ")
+    
+    """
     if decimal == 1:
         horizontal("b")
         vertical("y")
@@ -28,6 +37,8 @@ def draw(decimal):
         vertical("y")
         horizontal("b")
         print(" ")
+    
+        
     elif decimal == 0:
         horizontal("a")
         vertical("x")
@@ -35,8 +46,8 @@ def draw(decimal):
         vertical("x")
         horizontal("a")
         print(" ")
-
-       
+    
+      
     elif decimal == 2:
         horizontal("a")
         vertical("y")
@@ -44,6 +55,8 @@ def draw(decimal):
         vertical("z")
         horizontal("a")
         print(" ")
+        [1,2,1,3,1]
+      
 
     elif decimal == 3:
         horizontal("a")
@@ -52,6 +65,7 @@ def draw(decimal):
         vertical("y")
         horizontal("a")
         print(" ")
+        
 
     elif decimal == 4:
         horizontal("c")
@@ -60,7 +74,7 @@ def draw(decimal):
         vertical("y")
         horizontal("b")
         print(" ")
-
+        
     elif decimal == 5:
         horizontal("a")
         vertical("z")
@@ -68,7 +82,7 @@ def draw(decimal):
         vertical("y")
         horizontal("a")
         print(" ")
-
+        
     elif decimal == 6:
         horizontal("a")
         vertical("y")
@@ -76,7 +90,7 @@ def draw(decimal):
         vertical("x")
         horizontal("a")
         print(" ")
-
+        
     elif decimal == 7:
         horizontal("a")
         vertical("y")
@@ -84,6 +98,8 @@ def draw(decimal):
         vertical("y")
         horizontal("b")
         print(" ")
+        
+    
     elif decimal == 8:
         horizontal("a")
         vertical("x")
@@ -91,6 +107,7 @@ def draw(decimal):
         vertical("x")
         horizontal("a")
         print(" ")
+        
     elif decimal == 9:
         horizontal("a")
         vertical("x")
@@ -98,15 +115,11 @@ def draw(decimal):
         vertical("y")
         horizontal("b")
         print(" ")
-draw(0)
-draw(1)
-draw(2)
-draw(3)
-draw(4)
-draw(5)
-draw(6)
-draw(7)
-draw(8)
-draw(9)
+"""
+for i in range(10):
+    print(i,"/n")
+    draw(i)        
+    
+
 
 
