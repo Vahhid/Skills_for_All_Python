3ae23c0ba97de8b8935e557e99a7400a2182b8c4
Vahhid Lelissa <vahhid@gmail.com>
2023-08-25 14:57:28 +0300

    Single digit display created

--- seven_segment_display.py
+++ seven_segment_display.py
@@ -1,29 +1,99 @@
 #This program will request the user to provide a number and display as a seven segment
-decimals = list(range(10))
-print(decimals)
+#decimals = list(range(10))
+#print(decimals)
 #Function for displaying individual decimals
 def draw(decimal):
-    #pattern for eight
-    """
-    ########
-    #      #
-    #      #
-    #      #
-    ########
-    #      #
-    #      #
-    #      #
-    ########
-    """
-    def horizontal():
-        print(8*"#")
-    def vertical():
-        for i in range(3):
-            print("#",4*" ","#")
-    horizontal()
-    vertical()
-    horizontal()
-    vertical()
-    horizontal()
-draw(8)
+    def horizontal(side):
+        if side =="solid":
+            print(8*"#")
+        elif side == "both":
+            print("#"," "*4 , "#")
+        elif side == "right":
+            print(" "," "*4 , "#")
+    def vertical(side):
+        if side == "both":
+            for i in range(3):
+                print("#",4*" ","#")
+        elif side == "right":
+            for i in range(3):
+                print(" ",4*" ","#")
+        elif side == "left":
+            for i in range(3):
+                print("#",4*" "," ")
+        else:
+             for i in range(3):
+                print(" ",4*" "," ")           
+    if decimal == 1:
+        horizontal("right")
+        vertical("right")
+        horizontal("right")
+        vertical("right")
+        horizontal("right")
+        print(" ")
+    elif decimal == 0:
+        horizontal("solid")
+        vertical("both")
+        horizontal("both")
+        vertical("both")
+        horizontal("solid")
+        print(" ")
+
+    elif decimal == 8:
+        horizontal("solid")
+        vertical("both")
+        horizontal("solid")
+        vertical("both")
+        horizontal("solid")
+        print(" ")
+        
+    elif decimal == 2:
+        horizontal("solid")
+        vertical("right")
+        horizontal("solid")
+        vertical("left")
+        horizontal("solid")
+        print(" ")
+
+    elif decimal == 3:
+        horizontal("solid")
+        vertical("right")
+        horizontal("solid")
+        vertical("right")
+        horizontal("solid")
+        print(" ")
+
+    elif decimal == 4:
+        horizontal("both")
+        vertical("both")
+        horizontal("solid")
+        vertical("right")
+        horizontal("right")
+        print(" ")
+
+    elif decimal == 5:
+        horizontal("solid")
+        vertical("left")
+        horizontal("solid")
+        vertical("right")
+        horizontal("solid")
+        print(" ")
+
+    elif decimal == 6:
+        horizontal("solid")
+        vertical("right")
+        horizontal("solid")
+        vertical("both")
+        horizontal("solid")
+        print(" ")
+
+    elif decimal == 7:
+        horizontal("solid")
+        vertical("right")
+        horizontal("right")
+        vertical("right")
+        horizontal("right")
+        print(" ")
+
+
+
 
