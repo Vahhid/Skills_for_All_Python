5edee2f2705ceff80dadfbdc8862d10bd671b66f
Vahhid Lelissa <vahhid@gmail.com>
2023-08-25 16:15:12 +0300

    Arguments shortened for ease of use

--- seven_segment_display.py
+++ seven_segment_display.py
@@ -4,96 +4,109 @@
 #Function for displaying individual decimals
 def draw(decimal):
     def horizontal(side):
-        if side =="solid":
+        if side =="a":
             print(8*"#")
-        elif side == "both":
-            print("#"," "*4 , "#")
-        elif side == "right":
+        elif side == "b":
             print(" "," "*4 , "#")
+        elif side == "c":
+            print("#"," "*4 , "#")
     def vertical(side):
-        if side == "both":
+        if side == "x":
             for i in range(3):
                 print("#",4*" ","#")
-        elif side == "right":
+        elif side == "y":
             for i in range(3):
                 print(" ",4*" ","#")
-        elif side == "left":
+        elif side == "z":
             for i in range(3):
                 print("#",4*" "," ")
-        else:
-             for i in range(3):
-                print(" ",4*" "," ")           
+          
     if decimal == 1:
-        horizontal("right")
-        vertical("right")
-        horizontal("right")
-        vertical("right")
-        horizontal("right")
+        horizontal("b")
+        vertical("y")
+        horizontal("b")
+        vertical("y")
+        horizontal("b")
         print(" ")
     elif decimal == 0:
-        horizontal("solid")
-        vertical("both")
-        horizontal("both")
-        vertical("both")
-        horizontal("solid")
+        horizontal("a")
+        vertical("x")
+        horizontal("c")
+        vertical("x")
+        horizontal("a")
         print(" ")
 
-    elif decimal == 8:
-        horizontal("solid")
-        vertical("both")
-        horizontal("solid")
-        vertical("both")
-        horizontal("solid")
-        print(" ")
-        
+       
     elif decimal == 2:
-        horizontal("solid")
-        vertical("right")
-        horizontal("solid")
-        vertical("left")
-        horizontal("solid")
+        horizontal("a")
+        vertical("y")
+        horizontal("a")
+        vertical("z")
+        horizontal("a")
         print(" ")
 
     elif decimal == 3:
-        horizontal("solid")
-        vertical("right")
-        horizontal("solid")
-        vertical("right")
-        horizontal("solid")
+        horizontal("a")
+        vertical("y")
+        horizontal("a")
+        vertical("y")
+        horizontal("a")
         print(" ")
 
     elif decimal == 4:
-        horizontal("both")
-        vertical("both")
-        horizontal("solid")
-        vertical("right")
-        horizontal("right")
+        horizontal("c")
+        vertical("x")
+        horizontal("a")
+        vertical("y")
+        horizontal("b")
         print(" ")
 
     elif decimal == 5:
-        horizontal("solid")
-        vertical("left")
-        horizontal("solid")
-        vertical("right")
-        horizontal("solid")
+        horizontal("a")
+        vertical("z")
+        horizontal("a")
+        vertical("y")
+        horizontal("a")
         print(" ")
 
     elif decimal == 6:
-        horizontal("solid")
-        vertical("right")
-        horizontal("solid")
-        vertical("both")
-        horizontal("solid")
+        horizontal("a")
+        vertical("y")
+        horizontal("a")
+        vertical("x")
+        horizontal("a")
         print(" ")
 
     elif decimal == 7:
-        horizontal("solid")
-        vertical("right")
-        horizontal("right")
-        vertical("right")
-        horizontal("right")
+        horizontal("a")
+        vertical("y")
+        horizontal("b")
+        vertical("y")
+        horizontal("b")
         print(" ")
-
-
+    elif decimal == 8:
+        horizontal("a")
+        vertical("x")
+        horizontal("a")
+        vertical("x")
+        horizontal("a")
+        print(" ")
+    elif decimal == 9:
+        horizontal("a")
+        vertical("x")
+        horizontal("a")
+        vertical("y")
+        horizontal("b")
+        print(" ")
+draw(0)
+draw(1)
+draw(2)
+draw(3)
+draw(4)
+draw(5)
+draw(6)
+draw(7)
+draw(8)
+draw(9)
 
 
