c8112efed6546b065251c233b0dde7998540385f
Vahhid Lelissa <vahhid@gmail.com>
2023-08-29 12:47:45 +0300

    Implemented with index method

--- 2_5_10_LAB_Find_a_word.py
+++ 2_5_10_LAB_Find_a_word.py
@@ -12,5 +12,18 @@ Hints:
 you should use the two-argument variants of the pos() functions inside your code;
 don't worry about case sensitivity.
 """
-word = input("Enter the word to be searched")
-phrase = input("Enter the character stream to check")
+in_word = input("Enter the word to be searched \n")
+in_phrase = input("Enter the character stream to check")
+def check_for_hidden (word, phrase):
+    counter = 0
+    subStrCounter = 0
+    while counter < len(word):
+        try:
+            subStrCounter = phrase[subStrCounter:].index(word[counter]) + 1
+            #print(word[counter], " located at ", subStrCounter-1)
+        except:
+            return "No"
+        counter +=1
+    return "Yes"
+
+print(check_for_hidden(in_word, in_phrase))
