da76cc8e177118e6d68da34dc0badb3f553fb12f
Vahhid Lelissa <vahhid@gmail.com>
2023-08-29 17:11:03 +0300

    Test inputs prepared

--- 2_5_11_LAB_Sudoku.py
+++ 2_5_11_LAB_Sudoku.py
@@ -13,6 +13,29 @@ reads 9 rows of the Sudoku, each containing 9 digits (check carefully if the dat
 outputs Yes if the Sudoku is valid, and No otherwise.
 Test your code using the data we've provided.
 
+yes:
+295743861
+431865927
+876192543
+387459216
+612387495
+549216738
+763524189
+928671354
+154938672
+
+no: 
+195743862
+431865927
+876192543
+387459216
+612387495
+549216738
+763524189
+928671354
+254938671
+
+"""
 """
 soduk = []
 
@@ -30,6 +53,46 @@ while count < 9:
             break
     count +=1
     soduk.append(line)
+"""    
+inOne = """295743861
+431865927
+876192543
+387459216
+612387495
+549216738
+763524189
+928671354
+154938672"""
+inTwo = """195743862
+431865927
+876192543
+387459216
+612387495
+549216738
+763524189
+928671354
+254938671
+"""
+def str_to_soduk(test1Str1):
+    test1 = []
+    for i in range(len(test1Str1)):
+        if (i+1) % 10 != 0:
+            #st = test1Str1[i],i
+            test1.append(int(test1Str1[i]))
+    count = 0
+    sodukk1 = [] 
+    while count < len(test1):
+        row = []
+        for i in range(9):
+            row.append(test1[count+i])
+        sodukk1.append(row)
+        count+=9
+    return sodukk1
 
-print(soduk)
-print(valid_out)
+gong1 = str_to_soduk(inOne)
+print("________________________________________")
+gong2 = str_to_soduk(inTwo)
+
+
+
+    
