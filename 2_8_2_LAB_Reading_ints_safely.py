271fe0c0af3802659b39ceea8cb592f383afd92c
Vahhid Lelissa <vahhid@gmail.com>
2023-08-31 07:11:38 +0300

    Comment and outline from course

---
+++ 2_8_2_LAB_Reading_ints_safely.py
@@ -0,0 +1,22 @@
+"""
+2.8.2   LAB   Reading ints safely
+Your task is to write a function able to input integer values and to check if they are within a specified range.
+
+The function should:
+
+accept three arguments: a prompt, a low acceptable limit, and a high acceptable limit;
+if the user enters a string that is not an integer value, the function should emit the message Error: wrong input, and ask the user to input the value again;
+if the user enters a number which falls outside the specified range, the function should emit the message Error: the value is not within permitted range (min..max) and ask the user to input the value again;
+if the input value is valid, return it as a result.
+
+"""
+def read_int(prompt, min, max):
+    #
+    # Write your code here.
+    #
+
+
+v = read_int("Enter a number from -10 to 10: ", -10, 10)
+
+print("The number is:", v)
+    
