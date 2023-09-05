dec9f34e21df837552fc21ef2c92e5d965b21d82
Vahhid Lelissa <vahhid@gmail.com>
2023-09-05 08:45:34 +0300

    Assignment ouline

---
+++ 3_2_8_LAB_Counting_stack.py
@@ -0,0 +1,16 @@
+"""
+Your task is to extend the Stack class behavior in such a way so that the class 
+    is able to count all the elements that are pushed and popped (we assume that 
+    counting pops is enough). Use the Stack class we've provided in the editor.
+
+Follow the hints:
+
+-introduce a property designed to count pop operations and name it in a way 
+    which guarantees it is hidden;
+-initialize it to zero inside the constructor;
+-provide a method which returns the value currently assigned to the counter 
+    (name it get_counter()).
+
+Complete the code in the editor. Run it to check whether your code outputs 100.
+
+"""
