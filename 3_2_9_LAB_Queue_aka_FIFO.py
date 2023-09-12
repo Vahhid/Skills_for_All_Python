b0dc45b5d6c51b0e7f5d7f8306a0cbfd2ec1c8e0
Vahhid Lelissa <vahhid@gmail.com>
2023-09-12 17:56:59 +0300

    Outline and requirment description

---
+++ 3_2_9_LAB_Queue_aka_FIFO.py
@@ -0,0 +1,63 @@
+"""
+3.2.9   LAB   Queue aka FIFO
+
+Let's try something new now. A queue is a data model characterized 
+    by the term FIFO: First In – Fist Out. Note: a regular queue 
+    (line) you know from shops or post offices works exactly in 
+    the same way – a customer who came first is served first too.
+
+Your task is to implement the Queue class with two basic operations:
+
+put(element), which puts an element at end of the queue;
+get(), which takes an element from the front of the queue and returns 
+    it as the result (the queue cannot be empty to successfully perform it.)
+Follow the hints:
+
+    use a list as your storage (just like we did with the stack)
+    put() should append elements to the beginning of the list, 
+        while get() should remove the elements from the end of the list;
+    
+    define a new exception named QueueError (choose an exception 
+        to derive it from) and raise it when get() tries to operate 
+        on an empty list.
+
+Expected output
+1
+dog
+False
+Queue error
+
+"""
+class QueueError(???):  # Choose base class for the new exception.
+    #
+    #  Write code here
+    #
+
+
+class Queue:
+    def __init__(self):
+        #
+        # Write code here
+        #
+
+    def put(self, elem):
+        #
+        # Write code here
+        #
+
+    def get(self):
+        #
+        # Write code here
+        #
+
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Queue error")
+    
